a0940c78a50eeacdf6d9ec688b0efbdc25f5ce78
ievaabro <ievaabromaityte11@gmail.com>
2026-01-28 13:09:08 +0100

    moved

---
+++ notes.and.lectures/hello.cc
@@ -0,0 +1,5 @@
+#include"hello.h"
+#include<iostream>
+void hello(void){
+	std::cout << "hello from hello" << "\n";
+	}
---
+++ notes.and.lectures/hello.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main(){
+    std::cout << "Howdy Partner" << std::endl;
+    return 0;
+}
